99e6836efe68a66109bae0ddcd69e985ad3c1b48
ArfinHassib <arfinhasib21375@gmail.com>
2023-05-25 17:25:08 +0600

    firstCommit

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+   {
+      title: 'LAMP Website',
+      description:
+         'Using HTML, CSS, & Javascript I have built this beautiful fluid, modern & responsive website. I have alsom used third party javascript animation library Scroll-Reveal and also icon library remix icon on the website.',
+      image: '/images/1.png',
+      tags: ['Html', 'CSS', 'Javascript'],
+      source: 'https://google.com',
+      visit: 'https://google.com',
+      id: 0,
+   },
+   {
+      title: 'Magic Match Game',
+      description:
+         "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+      image: '/images/2.png',
+      tags: ['React', 'JavaScript'],
+      source: 'https://google.com',
+      visit: 'https://google.com',
+      id: 1,
+   },
+   {
+      title: 'Cinemate Movie Database',
+      description:
+         "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+      image: '/images/3.png',
+      tags: ['React', 'Javascript'],
+      source: 'https://google.com',
+      visit: 'https://google.com',
+      id: 2,
+   },
+   {
+      title: 'Unichat',
+      description:
+         'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
+      image: '/images/4.jpg',
+      tags: ['React', 'ChatEngine', 'Firebase'],
+      source: 'https://google.com',
+      visit: 'https://google.com',
+      id: 3,
+   },
+];
+
+export const TimeLineData = [
+   { year: 2020, text: 'Started my journey' },
+   { year: 2021, text: 'Worked as a freelance developer' },
+   { year: 2021, text: 'Masterd UI & UX Design' },
+   { year: 2022, text: 'Mastered React, Next & Typescript' },
+   { year: 2023, text: 'Started My Own Youtube Channel - Code Ninja' },
+];
